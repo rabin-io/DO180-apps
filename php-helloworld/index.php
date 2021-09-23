72ae1c32ca8853182a12d63421826a0eebf9e17c
Student User <student@workstation.lab.example.com>
2021-09-23 13:40:52 -0400

    update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,8 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-?>
+
+ print "Hello, World! php version is " . PHP_VERSION . "\n";
+ print "You are connected from " . $_SERVER['REMOTE_ADDR'] . "\n";
+
+ echo "<pre>"
+	 print_r($_SERVER);
+ echo "</pre>"
